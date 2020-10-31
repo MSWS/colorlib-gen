939b22be970d14b635535b1ff22ec2d1dae4f2e5
c0rp3n <ojhitchcock@gmail.com>
2020-10-31 19:16:21 +0000

    [Travis] Use Catch2 v2 branch

--- scripts/install-catch2.sh
+++ scripts/install-catch2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone https://github.com/catchorg/Catch2.git
+git clone -branch v2.x https://github.com/catchorg/Catch2.git
 mkdir Catch2/build
 cd Catch2/build
 cmake -DBUILD_TESTING=OFF ..
